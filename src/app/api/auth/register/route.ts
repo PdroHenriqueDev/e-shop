43f8d6690120f06153e54bbd25edbd508a3a928e
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2024-07-13 13:27:20 -0300

    feat(prisma):  name field

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -4,12 +4,12 @@ import {sql} from '@vercel/postgres';
 
 export async function POST(request: Request) {
   try {
-    const {email, password} = (await request.json()) as any;
+    const {name, email, password} = (await request.json()) as any;
 
     const hashedPassword = await hash(password, 10);
 
     const response =
-      await sql`INSERT INTO users (email, password) VALUES (${email}, ${hashedPassword})`;
+      await sql`INSERT INTO users (name, email, password) VALUES (${name}, ${email}, ${hashedPassword})`;
 
     return NextResponse.json({message: 'success'});
   } catch (e) {
