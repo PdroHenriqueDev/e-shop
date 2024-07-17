ae1e2f3939f2f7c867496d1a543c3180d7dfd618
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2024-07-17 18:10:57 -0300

    feat: auth logic

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,29 @@
+import {NextRequest, NextResponse} from 'next/server';
+import {compare} from 'bcrypt';
+import prisma from '@/lib/prisma';
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const {email, password} = await req.json();
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({message: 'Invalid credentials'}, {status: 401});
+    }
+
+    const isValid = await compare(password!, user.password);
+
+    if (!isValid) {
+      return NextResponse.json({message: 'Invalid credentials'}, {status: 401});
+    }
+
+    return Response.json({user}, {status: 200});
+  } catch (e) {
+    console.log({e});
+    return NextResponse.json({message: 'error'});
+  }
+}
