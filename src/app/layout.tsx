cbe5c9a57ac66927397418229120e53590ab19ba
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2024-08-10 19:32:01 -0300

    refactor(auth): adjust NextAuthProvider to correctly type and handle session

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,14 +13,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  session,
 }: Readonly<{
   children: React.ReactNode;
-  session: Session | null;
 }>) {
   return (
     <html lang="en">
-      <NextAuthProvider session={session}>
+      <NextAuthProvider>
         <body className={`${inter.className} bg-gray-100`}>
           <NotificationProvider>{children}</NotificationProvider>
         </body>
