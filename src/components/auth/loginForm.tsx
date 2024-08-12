b70158d4b3f8188bbc6c4deffe6d9f4160a7a3bf
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2024-08-12 17:50:59 -0300

    refactor: redirect when login

--- src/components/auth/loginForm.tsx
+++ src/components/auth/loginForm.tsx
@@ -43,7 +43,6 @@ export default function LoginForm() {
     const {email, password} = user;
 
     const result = await signIn('credentials', {
-      redirect: false,
       callbackUrl: '/',
       email,
       password,
