8245015d88d6c2b1aad741ca6a0896563a22e282
pedrohenriquedev28 <pedrohenriquedev@outlook.com>
2024-09-02 17:42:13 -0300

    fix(auth): correct export structure for authOptions in NextAuth route

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -2,7 +2,7 @@ import {NextResponse} from 'next/server';
 import prisma from '@/lib/prisma';
 import {User} from '@prisma/client';
 import {getServerSession} from 'next-auth';
-import {authOptions} from '@/app/api/auth/[...nextauth]/route';
+import {authOptions} from '../auth/[...nextauth]/authOptions';
 
 export async function POST(request: Request) {
   try {
