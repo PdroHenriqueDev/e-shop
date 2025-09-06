fd22ef5deca58ade9a26340abcc0678e7281623e
PdroHenriqueDev <pedrohenriquedev28@gmail.com>
2025-09-06 12:29:31 -0300

    fix: correct typo in layout className

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-priamry`}>
+      <body className={`${inter.className} bg-primary`}>
         <NextAuthProvider>
           <AntdRegistry>
             <App>
